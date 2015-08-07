5918407a9aa223864d7193213eba402ca7a339c7
gygias <gygias@users.noreply.github.com>
2015-08-06 19:57:37 -0700

    Initial Commit

---
+++ fame/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  fame
+//
+
+//  Copyright (c) 2015 Combobulated Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
